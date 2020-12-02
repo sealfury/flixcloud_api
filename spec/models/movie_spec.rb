dd633f972077ac2df2a6b343bffc22d8e72cca14
William <willspecker@gmail.com>
2020-12-02 10:00:32 +0100

    Creates spec test for movie model with one validation

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,18 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
 RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '\'Movies\' has db columns' do
+    it { is_expected.to have_db_column :title }
+  end
+
+    describe 'Validations' do
+      it { is_expected.to validate_presence_of :title }
+    end
+
+    describe 'Factory' do
+      it 'is expected to have a valid Factory' do
+        expect(create(:movie)).to be_valid
+    end
+  end
 end
