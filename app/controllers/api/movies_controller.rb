c5b645e9dd4ce312560ee5051f3878371692f236
flis_ann <ann.flismark@gmail.com>
2020-12-02 21:33:38 +0100

    Creating fixture test. GET request to restfull api movie.titles.json file with movies data from our external api Removing api key, this will break test. Need to fix new urls Added two GEMS, webmock and rest-client

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,6 +1,32 @@
 class Api::MoviesController < ApplicationController
   def index
-    movies = Movie.all 
+    # movies = Movie.all ### TA BORT NYCKELN, lägg i credentials istället. 
+    result = RestClient.get()
+    body = JSON.parse(result.body)
+    movies = sanitized_movies(body)  
+
     render json: { movies: movies }, status: 200
   end
+
+  private
+  def sanitized_movies(body)
+    sanitized_movies = []
+    array = body["results"]
+    array.each do |movie|
+    sanitized_movies.push format_movie(movie)
+    end
+    sanitized_movies
+  end
+
+  def format_movie(movie)
+    {
+      title: movie["title"]
+# poster: add to test, add to db
+# releasedate: add to test, add to db
+# genre: add to test, add to db
+# Take care of key in two places
+
+    }
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :movies, only: [:index]
+
   end
 end
