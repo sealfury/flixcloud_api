f4d848b631d07c58f2452f8be31afdf671723541
William <willspecker@gmail.com>
2020-12-03 17:17:20 +0100

    fixing stup in rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,13 +26,14 @@ RSpec.configure do |config|
   config.before(:each) do
     fixture_file = File.open("#{fixture_path}/movie_titles.json").read
     stub_request(:get, "https://api.themoviedb.org/3/discover/movie?api_key=#{Rails.application.credentials.moviedb[:api_key]}&include_adult=false&language=en-US&page=1&sort_by=popularity.desc").
-         with(
-           headers: {
-       	  'Accept'=>'*/*',
-       	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-       	  'Host'=>'api.themoviedb.org',
-       	  'User-Agent'=>'rest-client/2.1.0 (darwin19.6.0 x86_64) ruby/2.5.1p57'
-           }).
-         to_return(status: 200, body: fixture_file, headers: {})
+      with(
+        headers: {
+        'Accept'=>'*/*',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'Host'=>'api.themoviedb.org',
+        'User-Agent'=>'rest-client/2.1.0 (linux-gnu x86_64) ruby/2.5.1p57'
+        }).
+      to_return(status: 200, body: fixture_file, headers: {})
+
   end
 end
